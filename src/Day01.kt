a65a158068aff489fe8c88299e25144489172102
Matthew Jacobs <mjacobs@arrivelogistics.com>
2022-12-06 09:04:18 -0600

    Day 1 star 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,10 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.asSequence()
+            .map { it.toIntOrNull() }
+            .chunkByNotNull()
+            .map { it.sum() }
+            .max()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +13,36 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part1Result = part1(testInput)
+    check(part1Result == 15)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun <T> Sequence<T?>.chunkByNotNull(): Sequence<List<T>> = sequence {
+    var grouping = mutableListOf<T>()
+    for (item in iterator()) {
+        if (item != null) {
+            grouping.add(item)
+        } else {
+            yield(grouping)
+            grouping = mutableListOf()
+        }
+    }
+    yield(grouping)
+}
+
+fun <T> Sequence<T>.chunkByPredicate(predicate: (T) -> Boolean): Sequence<List<T>> = sequence {
+    var grouping = mutableListOf<T>()
+    for (item in iterator()) {
+        if (predicate(item)) {
+            grouping.add(item)
+        } else {
+            yield(grouping)
+            grouping = mutableListOf()
+        }
+    }
+    yield(grouping)
 }
